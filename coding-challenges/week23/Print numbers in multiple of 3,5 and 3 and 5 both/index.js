d9066e45c9293dafbe28e52b0281c3d83a0d861d
Deepak Shrivastava <dpkshrivastava10@gmail.com>
2019-07-31 09:43:39 +0530

    Print Fizz and BUZZ in multiple of 3 and 5

--- coding-challenges/week23/Print numbers in multiple of 3,5 and 3 and 5 both/index.js
+++ coding-challenges/week23/Print numbers in multiple of 3,5 and 3 and 5 both/index.js
@@ -4,15 +4,15 @@
 function numbers() {
 
         for (let i = 1; i <= 100; i++){
-            if (i%3 === 0) {
+            if (i%3 == 0 && i%5 != 0) {
                 console.log("FIZZ");
             }
 
-            if(i%5 === 0) {
+            else if (i%5 == 0 && i%3 != 0) {
                 console.log("BUZZ");
             }
 
-            if (i%3 === 0 && i%5 === 0) {
+            else if (i%3 == 0 && i%5 == 0) {
                 console.log("FIZZBUZZ");
             }
             else {
