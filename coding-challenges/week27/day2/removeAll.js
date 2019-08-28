69c24e426a7b608f440a6b7ebe1d3fee1ebaf87a
Deepak Shrivastava <dpkshrivastava10@gmail.com>
2019-08-28 10:59:09 +0530

    challenges

---
+++ coding-challenges/week27/day2/removeAll.js
@@ -0,0 +1,16 @@
+const LinkedList = require ("./linkedList");
+
+class RemoveOperation {
+     removeAll() {
+
+    while(Node) {
+       l.delete("chennai");
+    }
+
+}
+}
+
+let l = new RemoveOperation();
+
+
+//removeAll("Deepak");
---
+++ coding-challenges/week27/day3/findElementInArray.js
@@ -0,0 +1,24 @@
+function missingNumbers(arr) {
+
+    let missingArr = [];
+    for (let i = 0; i < arr.length-1; i++) {
+        let diff = arr[i+1] - arr[i];
+      //  console.log(arr[i+1], arr[i]);
+        if (diff != 1) {
+            while(arr[i+1] - arr[i] != 1){
+                missingArr.push(arr[i]+1); 
+                arr[i] += 1;
+            }
+         }
+        else {
+           continue;
+        }
+    }
+    return missingArr;
+}
+
+
+let arr1 = [1,2,3,4,5,9,10];
+let arr2 = [1,2,3,4,5,6,8,10,11,13,14,17];
+console.log(missingNumbers(arr1));
+console.log(missingNumbers(arr2));
