85cf64fe3a1a1bea45601ff0009182c5881df753
Deepak Shrivastava <dpkshrivastava10@gmail.com>
2019-09-10 10:40:55 +0530

    Challenges

--- coding-challenges/week29/day2/reverseDigits.js
+++ coding-challenges/week29/day2/reverseDigits.js
@@ -3,24 +3,20 @@
 let rev = 0;
 function reverseDigits(n) {
     let rem = 0;
-   if(n > 0) {
+   if(n > 0 || n < 0) {
        rem = n % 10;
        rev = rev * 10 + rem;
-       n = Math.floor(n / 10);
-       reverseDigits(n);       
-   }
-   if(n < 0) {
-        rem = n % 10;
-      
-        rev = rev * 10 + rem;
-        
+       if(n > 0) {
+        n = Math.floor(n / 10);
+       }
+       if(n < 0) {
         n = Math.trunc(n / 10);
-        
-        reverseDigits(n);  
+       }
+       reverseDigits(n);
    }
    
     return rev;
 }
 
 //console.log(reverseDigits(120));
-console.log(reverseDigits(-1234));
+console.log(reverseDigits(1234));
